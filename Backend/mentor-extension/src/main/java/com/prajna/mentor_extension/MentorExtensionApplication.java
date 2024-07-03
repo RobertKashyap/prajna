54403a9bc12682b21f48400bce3cd1dd53490d17
Robert Kashyap <orionalchemist@gmail.com>
2024-07-03 22:58:04 +0530

    backend springsetup

---
+++ Backend/mentor-extension/src/main/java/com/prajna/mentor_extension/MentorExtensionApplication.java
@@ -0,0 +1,13 @@
+package com.prajna.mentor_extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorExtensionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorExtensionApplication.class, args);
+	}
+
+}
