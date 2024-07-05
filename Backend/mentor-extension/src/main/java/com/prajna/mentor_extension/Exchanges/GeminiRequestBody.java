38f6fbc2d5a472ba8fef73e3f222f9c3907f682c
Robert Kashyap <orionalchemist@gmail.com>
2024-07-05 17:16:37 +0530

    DTO's config

---
+++ Backend/mentor-extension/src/main/java/com/prajna/mentor_extension/Exchanges/GeminiRequestBody.java
@@ -0,0 +1,81 @@
+package com.prajna.mentor_extension.Exchanges;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeminiRequestBody {
+    private List<Content> contents;
+    private List<SafetySetting> safetySettings;
+    private GenerationConfig generationConfig;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Content {
+        private List<Part> parts;
+
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class Part {
+            private String text;
+        }
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SafetySetting {
+        private String category;
+        private String threshold;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GenerationConfig {
+        private List<String> stopSequences;
+        private double temperature;
+        private int maxOutputTokens;
+        private double topP;
+        private int topK;
+        private String responseMimeType;
+        private ResponseSchema responseSchema;
+
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public static class ResponseSchema {
+            private String type;
+            private Item items;
+
+            @Data
+            @NoArgsConstructor
+            @AllArgsConstructor
+            public static class Item {
+                private String type;
+                private Properties properties;
+
+                @Data
+                @NoArgsConstructor
+                @AllArgsConstructor
+                public static class Properties {
+                    private RecipeName recipe_name;
+
+                    @Data
+                    @NoArgsConstructor
+                    @AllArgsConstructor
+                    public static class RecipeName {
+                        private String type;
+                    }
+                }
+            }
+        }
+    }
+}
